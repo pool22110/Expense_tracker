3265cc8862d9ffeb3fa735b3d1b71caa17467283
Prem <premkhodke.99@gmail.com>
2024-09-11 14:45:35 +0530

    component state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,52 @@ import PiePresentation from "@/components/PiePresentation";
 import TopExpense from "@/components/TopExpense";
 import { store } from "@/store/store";
 import { Provider } from "react-redux";
+import { useState } from "react";
+import Modals from "@/components/Modals";
 
 export default function Home() {
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [modalType, setModalType] = useState<"INCOME" | "EXPENSE" | "">("");
+
+  const handleCloseModal = (): void => {
+    setShowModal((prev) => !prev);
+  };
+
   return (
     <Provider store={store}>
       <main className="py-3 px-8">
+        {modalType === "INCOME" ? (
+          <Modals
+            handleCloseModal={handleCloseModal}
+            showModal={showModal}
+            type="INCOME"
+            setModalType={setModalType}
+          />
+        ) : (
+          <Modals
+            handleCloseModal={handleCloseModal}
+            showModal={showModal}
+            type="EXPENSE"
+            setModalType={setModalType}
+          />
+        )}
+
         <h1 className="text-[32px] font-bold leading-9">Expense Tracker</h1>
         <div className="w-full h-max p-6 rounded-[10px] bg-[#626262] mt-[9px] flex justify-between">
-          <Card title="Wallet Balance" amount={500} type="INCOME"/>
-          <Card title="Expenses" amount={500} type="EXPENSE"/>
+          <Card
+            title="Wallet Balance"
+            amount={500}
+            type="INCOME"
+            handleCloseModal={handleCloseModal}
+            setModalType={setModalType}
+          />
+          <Card
+            title="Expenses"
+            amount={500}
+            type="EXPENSE"
+            handleCloseModal={handleCloseModal}
+            setModalType={setModalType}
+          />
           <PiePresentation />
         </div>
         <TopExpense />
